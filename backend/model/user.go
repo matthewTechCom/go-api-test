5553553422ab2a14973535451c2963b419b0dfb5
Yuhri <matthewyuh246@gmail.com>
2025-03-18 18:00:17 +0900

    fix type

--- backend/model/user.go
+++ backend/model/user.go
@@ -1,8 +1,8 @@
 package model
 
 type User struct {
-	ID       int      `json:"id"`
-	Email    string   `json:"email"`
-	Name     string   `json:"name"`
-	GroupIDs []string `json:"group_ids,omitempty"`
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	GroupIDs []int  `json:"group_ids,omitempty"`
 }
