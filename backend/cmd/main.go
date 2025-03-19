101ab34ce3fd19230568609b87faafba342c5b30
Yuhri <matthewyuh246@gmail.com>
2025-03-19 23:28:45 +0900

    main.goの修正

---
+++ backend/cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Error loading .env file")
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Echo!")
+	})
+	e.Start(":8080")
+}
