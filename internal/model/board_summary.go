3fd0c6e5e365df984f8ff24b502f54863a601e1d
annyouu <165882043+annyouu@users.noreply.github.com>
2025-03-21 01:09:29 +0900

    Delete internal directory

--- internal/model/board_summary.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "time"
-
-type BoardSummary struct {
-	ID int `json:"id"`
-	Summary string `json:"summary"`
-	CreatedAt time.Time `json:"created_at"`
-}
--- internal/model/group.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Group struct {
-	ID        int    `json:"id"`
-	Name      string `json:"name"`
-	MemberIDs []int  `json:"member_ids"`
-}
